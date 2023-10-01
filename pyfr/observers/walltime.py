4c794fa202af23e891178e29cadc1b5531506eb8
entropy <sambit98@tamu.edu>
2023-10-01 14:38:01 -0500

    Output walltime for a physical time-step

--- pyfr/observers/walltime.py
+++ pyfr/observers/walltime.py
@@ -16,5 +16,26 @@ class WallTime(BaseCost):
     def __init__(self, intg, cfgsect, suffix=None):
         super().__init__(intg, cfgsect, suffix)
 
+
+
     def __call__(self, intg):
-        self.plot_intg_cost(intg.costs[f'walltime'], name = self.plot_name, if_log = False)
+
+        if self.if_plot:
+            self.plot_intg_cost(intg.costs[f'walltime'], 
+                                name = self.plot_name, 
+                                if_log = False)
+
+        if self.outf:
+            # Flatten the cost array
+            # We need one entry per __call__
+            # So eahc entry is a list separaterd with ','
+            costs = intg.costs[f'walltime'].flatten()
+            
+            # Get the overall sum
+            costs = sum(costs) 
+            print(costs, 
+                  sep=',',
+                file=self.outf)
+
+            # Flush to disk
+            self.outf.flush()
