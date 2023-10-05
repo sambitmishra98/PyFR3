277f3f36173a6bf44ac8cef12ea5d769ee5d7539
sambit_aces <sambit98@gmail.com>
2023-10-05 10:33:40 -0500

    Annoying changes from PyLint

--- pyfr/observers/psmoothing.py
+++ pyfr/observers/psmoothing.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from pyfr.observers import BaseParameter
 
+
 class PSmoothing(BaseParameter):
     name = 'psmoothing'
     systems = ['*']
@@ -9,24 +10,20 @@ class PSmoothing(BaseParameter):
 
     def __init__(self, intg, cfgsect, suffix=None):
         super().__init__(intg, cfgsect, suffix)
-    
+
         if intg.parameters.get(self.parameter_name) is None:
 
             print(f"Not provided {self.parameter_name} in config file. "
-                    f"Using default value.")
+                  f"Using default value.")
 
             _, cstepsf = zip(*self.cfg.getliteral(
                 'solver-dual-time-integrator-multip', 'cycle'))
 
             def_csteps = cstepsf[int(suffix)]
             intg.parameters[self.parameter_name] = def_csteps * np.ones(
-                    (
-                    self._stages, 
-                    self._levels,
-                    self._pniters, 
-                    ))
+                    (self._stages, self._levels, self._pniters, ))
 
-        #print(f"NEW: {intg.parameters[self.parameter_name]}")
+        # print(f"NEW: {intg.parameters[self.parameter_name]}")
 
     def __call__(self, intg):
         return
