a2d11eb975295f91abab0f4009f6c1a0a9d535f2
sambit_aces <sambit98@gmail.com>
2023-10-11 12:38:50 -0500

    replace names

--- pyfr/plugins/pseudodtstats.py
+++ pyfr/plugins/pseudodtstats.py
@@ -21,7 +21,7 @@ class PseudoDtStatsPlugin(BaseSolnPlugin):
         self.e_types = intg.system.ele_types
 
         # Maximum of 3 Levels of abstraction for the stats of pseudo-dt field
-        intg.Δτ_stats = { 'n' : {'all':0},
+        intg.dtau_stats = { 'n' : {'all':0},
                          'res': {'all':0}|{p:{'all':0}                                     
                                            for p in self.fvars}, 
  
@@ -44,7 +44,7 @@ class PseudoDtStatsPlugin(BaseSolnPlugin):
                 'level', intg.cfg.getint('solver','order'))
 
         csv_header =  'pseudo-steps, tcurr'
-        for k, v in intg.Δτ_stats.items():
+        for k, v in intg.dtau_stats.items():
             if k != 'all':
                 csv_header += f',{k}'
                 for vk, vv in v.items():
@@ -74,9 +74,9 @@ class PseudoDtStatsPlugin(BaseSolnPlugin):
 
             if iternr == 1:           # We can store the last step's data
                 if 'solver-dual-time-integrator-multip' in intg.cfg.sections():
-                    Δτ_mats = intg.pseudointegrator.pintgs[self.level].Δτ_mats
+                    dtau_mats = intg.pseudointegrator.pintgs[self.level].dtau_mats
                 else:
-                    Δτ_mats = intg.pseudointegrator.Δτ_mats
+                    dtau_mats = intg.pseudointegrator.dtau_mats
                 
                 if self.last_appendable != None:
                     if self.stored_tₚ != self.tₚ:
@@ -87,12 +87,12 @@ class PseudoDtStatsPlugin(BaseSolnPlugin):
 
                 self.stored_tₚ = self.tₚ
 
-            intg.Δτ_stats['n']['all'] = npiter-self.prev_npiter
+            intg.dtau_stats['n']['all'] = npiter-self.prev_npiter
 
-            self.Δτ_statistics(intg, Δτ_mats)
+            self.dtau_statistics(intg, dtau_mats)
             self.residual_statistics(intg, resid)
             self.last_appendable = (npiter, intg.tcurr, 
-                                    *self.Δτ_stats_as_list(intg.Δτ_stats))
+                                    *self.dtau_stats_as_list(intg.dtau_stats))
 
         self.tₚ = intg.tcurr
 
@@ -108,9 +108,9 @@ class PseudoDtStatsPlugin(BaseSolnPlugin):
         # Reset the stats
         self.stats = []
 
-    def Δτ_statistics(self, intg, Δτ_mats):
+    def dtau_statistics(self, intg, dtau_mats):
         '''
-            self.Δτ_mats is a list of matrices, one for each element type.
+            self.dtau_mats is a list of matrices, one for each element type.
             Each matrix is a 3D array of shape (nupts, nvars, neles)
         '''
 
@@ -119,49 +119,49 @@ class PseudoDtStatsPlugin(BaseSolnPlugin):
 
                 # each element type, each soln point in element, each variable in (p, u, v, w)
                 # Stats obtained over all elements
-                intg.Δτ_stats['min'][var][e_type]['each'] = (
-                                                    Δτ_mats[i][:, j, :].min(1))
-                intg.Δτ_stats['max'][var][e_type]['each'] = (
-                                                    Δτ_mats[i][:, j, :].max(1))
+                intg.dtau_stats['min'][var][e_type]['each'] = (
+                                                    dtau_mats[i][:, j, :].min(1))
+                intg.dtau_stats['max'][var][e_type]['each'] = (
+                                                    dtau_mats[i][:, j, :].max(1))
 
                 # each element type, each variable in (p, u, v, w)
                 # Stats obtained over all elements and element soln points
 
-                intg.Δτ_stats['min'][var][e_type]['all'] = (
-                                intg.Δτ_stats['min'][var][e_type]['each'].min())
-                intg.Δτ_stats['max'][var][e_type]['all'] = (
-                                intg.Δτ_stats['max'][var][e_type]['each'].max())
+                intg.dtau_stats['min'][var][e_type]['all'] = (
+                                intg.dtau_stats['min'][var][e_type]['each'].min())
+                intg.dtau_stats['max'][var][e_type]['all'] = (
+                                intg.dtau_stats['max'][var][e_type]['each'].max())
 
             # each variable in (p, u, v, w)
             # Stats obtained over all element types, elements and element soln points
-            intg.Δτ_stats['min'][var]['all'] = min(
-                [intg.Δτ_stats['min'][var][e_type]['all'] 
+            intg.dtau_stats['min'][var]['all'] = min(
+                [intg.dtau_stats['min'][var][e_type]['all'] 
                                                     for e_type in self.e_types])
-            intg.Δτ_stats['max'][var]['all'] = max(
-                [intg.Δτ_stats['max'][var][e_type]['all'] 
+            intg.dtau_stats['max'][var]['all'] = max(
+                [intg.dtau_stats['max'][var][e_type]['all'] 
                                                     for e_type in self.e_types])
 
-            tₘₐₓ = np.array(intg.Δτ_stats['min'][var]['all'])
+            tₘₐₓ = np.array(intg.dtau_stats['min'][var]['all'])
             if self.rank != self.root:
                 self.comm.Reduce(tₘₐₓ       , None , op=mpi.MIN, root=self.root)
             else:
                 self.comm.Reduce(mpi.IN_PLACE, tₘₐₓ, op=mpi.MIN, root=self.root)
-            intg.Δτ_stats['min'][var][e_type]['all'] = tₘₐₓ
+            intg.dtau_stats['min'][var][e_type]['all'] = tₘₐₓ
 
-            tₘᵢₙ = np.array(intg.Δτ_stats['max'][var]['all'])
+            tₘᵢₙ = np.array(intg.dtau_stats['max'][var]['all'])
             if self.rank != self.root:
                 self.comm.Reduce(tₘᵢₙ       , None , op=mpi.MAX, root=self.root)
             else:
                 self.comm.Reduce(mpi.IN_PLACE, tₘᵢₙ, op=mpi.MAX, root=self.root)
-            intg.Δτ_stats['max'][var][e_type]['all'] = tₘᵢₙ
+            intg.dtau_stats['max'][var][e_type]['all'] = tₘᵢₙ
 
         # Stats obtained over 
         #   all element types, elements, variable 
         #   in (p, u, v, w) 
         #   and element soln points
-        intg.Δτ_stats['min']['all'] = min([intg.Δτ_stats['min'][var]['all'] 
+        intg.dtau_stats['min']['all'] = min([intg.dtau_stats['min'][var]['all'] 
                                            for var in self.fvars])
-        intg.Δτ_stats['max']['all'] = max([intg.Δτ_stats['max'][var]['all'] 
+        intg.dtau_stats['max']['all'] = max([intg.dtau_stats['max'][var]['all'] 
                                            for var in self.fvars])
 
     def residual_statistics(self, intg, resid):
@@ -173,13 +173,13 @@ class PseudoDtStatsPlugin(BaseSolnPlugin):
 
         # each variable in (p, u, v, w)
         for j, var in enumerate(self.fvars):
-            intg.Δτ_stats['res'][var]['all'] = resid[j]
+            intg.dtau_stats['res'][var]['all'] = resid[j]
 
         if self.cfg.get('solver-time-integrator','pseudo-resid-norm')=='uniform':
-            intg.Δτ_stats['res']['all'] = max([intg.Δτ_stats['res'][var]['all'] 
+            intg.dtau_stats['res']['all'] = max([intg.dtau_stats['res'][var]['all'] 
                                            for var in self.fvars])
         elif self.cfg.get('solver-time-integrator', 'pseudo-resid-norm')=='l2':
-            intg.Δτ_stats['res']['all'] = sum([intg.Δτ_stats['res'][var]['all'] 
+            intg.dtau_stats['res']['all'] = sum([intg.dtau_stats['res'][var]['all'] 
                                            for var in self.fvars])
         else:
             raise ValueError('Unknown time integrator ')
@@ -195,12 +195,12 @@ class PseudoDtStatsPlugin(BaseSolnPlugin):
 
                 # each element type, each variable in (p, u, v, w)
                 # Stats obtained over all elements
-                intg.Δτ_stats['res'][var][e_type]['each'] = resid[i][j]
+                intg.dtau_stats['res'][var][e_type]['each'] = resid[i][j]
 
                 # each element type, each variable in (p, u, v, w)
                 # Stats obtained over all elements and element soln points
 
-                res = np.array(intg.Δτ_stats['res'][var][e_type]['each'].max())
+                res = np.array(intg.dtau_stats['res'][var][e_type]['each'].max())
 
                 if self.rank != self.root:
                     self.comm.Reduce(res, None , op=mpi.SUM, 
@@ -209,32 +209,32 @@ class PseudoDtStatsPlugin(BaseSolnPlugin):
                     self.comm.Reduce(mpi.IN_PLACE, res, op=mpi.SUM, 
                                      root = self.root)
 
-                intg.Δτ_stats['res'][var][e_type]['all'] = res
+                intg.dtau_stats['res'][var][e_type]['all'] = res
 
             # each variable in (p, u, v, w)
             # Stats obtained over all element types, elements and element soln points
-            intg.Δτ_stats['res'][var]['all'] = max([intg.Δτ_stats['res'][var][e_type]['all'] 
+            intg.dtau_stats['res'][var]['all'] = max([intg.dtau_stats['res'][var][e_type]['all'] 
                                                     for e_type in self.e_types])
 
         # Stats obtained 
         #   over all element types, elements, variable 
         #   in (p, u, v, w) 
         #   and element soln points
-        intg.Δτ_stats['res']['all'] = max([intg.Δτ_stats['res'][var]['all'] 
+        intg.dtau_stats['res']['all'] = max([intg.dtau_stats['res'][var]['all'] 
                                            for var in self.fvars])
 
-    def Δτ_stats_as_list(self, Δτ_stats):
-        Δτ_stats_list = []
-        for v in Δτ_stats.values():
-            Δτ_stats_list.append(v['all'])               
+    def dtau_stats_as_list(self, dtau_stats):
+        dtau_stats_list = []
+        for v in dtau_stats.values():
+            dtau_stats_list.append(v['all'])               
             for vk, vv in v.items():
                 if self.abstraction>1:                   
                     if isinstance(vv, dict):    
-                        Δτ_stats_list.append(vv['all'])
+                        dtau_stats_list.append(vv['all'])
                 if vk != 'all':
                     for vvv in vv.values():
                         if self.abstraction>2:
                             if isinstance(vvv, dict):
-                                Δτ_stats_list.append(vvv['all'])
+                                dtau_stats_list.append(vvv['all'])
 
-        return Δτ_stats_list
+        return dtau_stats_list
