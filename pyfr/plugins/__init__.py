a6adc151894706d98cb5701a02bb388091400b98
sambit98-spitfire <sambit98@tamu.edu>
2024-08-01 21:42:57 -0500

    Add functionality to modify configuration.

--- pyfr/plugins/__init__.py
+++ pyfr/plugins/__init__.py
@@ -19,6 +19,7 @@ from pyfr.util import subclass_where
 
 from pyfr.plugins.optimisationstats import OptimisationStatsPlugin
 from pyfr.plugins.bayesianoptimisation import BayesianOptimisationPlugin
+from pyfr.plugins.modifyconfiguration  import ModifyConfigPlugin
 from pyfr.plugins.rewind import RewindPlugin
 
 def get_plugin(prefix, name, *args, **kwargs):
